09ca09dbf8d266e64afe953151932f22a9a1333d
Lucas Dupin <dupin@google.com>
2020-03-14 14:44:40 -0700

    montana: Enable blurs, under dev-option
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I3352b34739f5f45efa4088044de0e030449ff866
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -6,6 +6,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-threads=6
 
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
+
 # GPU
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610

3f15da32e216bb5c2bad9eba16a7de2273bf0273
Jarl-Penguin <jarlpenguin@outlook.com>
2020-10-27 14:34:01 +0300

    montana: Import thermal engine config from stock
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- device.mk
+++ device.mk
@@ -99,6 +99,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.montana

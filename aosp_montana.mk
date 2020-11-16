4b23ebe99ff322df3491d656c1fb0bc628c93b68
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-11-16 18:45:01 +0000

    montana: Aosp

--- aosp_montana.mk
+++ aosp_montana.mk
@@ -10,11 +10,20 @@ $(call inherit-product, device/motorola/montana/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Google Apps
+TARGET_GAPPS_ARCH := arm64
+TARGET_MINIMAL_APPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_montana
+PRODUCT_NAME := aosp_montana
 PRODUCT_DEVICE := montana
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto G (5S)

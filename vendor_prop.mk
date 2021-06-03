88e10f9480e2ec577438d4464335e07974bc15ce
Jeferson <jroliveira.oliveira301@gmail.com>
2021-06-03 14:57:45 +0200

    montana: Rework dalvik props
    * Also enable dex2oat64
    
    Change-Id: I5f50ddbdcf4b623f147638c7414f16fc5e6b5af5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -4,7 +4,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-threads=6
+    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-cpu-set=4,5,6,7 \
+    dalvik.vm.dex2oat64.enabled=true
 
 # GPU
 PRODUCT_PROPERTY_OVERRIDES += \
